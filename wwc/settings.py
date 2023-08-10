9606cbdfae112da8913127849e87053738b7e592
Nyuydine Bill leynyuy <billleynyuy@gmail.com>
2023-08-10 09:42:30 +0100

    required

--- wwc/settings.py
+++ wwc/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles")
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
