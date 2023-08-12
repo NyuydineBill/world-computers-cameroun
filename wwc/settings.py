e3f35a376409549c70a9633c1e41d77dde90e9f2
Nyuydine Bill leynyuy <billleynyuy@gmail.com>
2023-08-12 01:04:28 +0100

    debug is true

--- wwc/settings.py
+++ wwc/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aku$$^6ga^gq@_ign#_d+q_2#d5la@_4jh)=+o8ci1$v+%kcr9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
